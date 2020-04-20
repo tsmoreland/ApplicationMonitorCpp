dfe7d8dbf4d15b70c1f75c5e16ce783bf8ce85e5
Terry Moreland <tsmoreland@live.com>
2020-04-21 00:08:23 +0100

    remove process iterable as it was a bit awkward, potentially more efficient but best not to prematurely optimize
    instead now getting all processes and storing them in a vector ~350 in a quick run through which isn't enough to worry too much about optimizations
    
    corrected a few minor bugs with move operations and verified process

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <filesystem>
-#include "Process.h"
+
 #include "EnvironmentService.h"
 
 using std::cout;
 using std::endl;
-using std::unique_ptr;
-using Shared::Domain::Process;
-using Shared::Domain::IProcess;
 using Shared::Services::EnvironmentService;
 
 int main()
 {
     try
     {
-        //const auto process = Process::Start(R"(c:\windows\system32\cmd.exe)", "/c sleep 5");
         EnvironmentService const environment{};
+
         auto const process = environment.StartProcess(R"(c:\windows\system32\cmd.exe)", "/c sleep 5");
         if (!process.has_value())
             return -1;
@@ -30,4 +27,3 @@ int main()
         cout << "Unexpected error occured: " << e.what() << endl;
     }
 }
-
