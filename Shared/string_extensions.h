a8cbf8898081c20c719ecb16fd7a109a74bc57c9
Terry Moreland <tsmoreland@live.com>
2020-04-23 01:01:07 +0100

    completed unit tests for string_equals and string_split

--- Shared/string_extensions.h
+++ Shared/string_extensions.h
@@ -19,16 +19,17 @@
 
 namespace std
 {
+
     template <typename TCHAR>
-    [[nodiscard]] bool string_equal(std::basic_string_view<TCHAR> const& leftHandSide, std::basic_string_view<TCHAR> const &rightHandSide, bool ignoreCase = false)
+    [[nodiscard]] bool string_equal(std::basic_string_view<TCHAR> const left_hand_side, std::basic_string_view<TCHAR> const right_hand_side, bool const ignoreCase = false)
     {
-        if (&leftHandSide == &rightHandSide)
+        if (&left_hand_side == &right_hand_side)
             return true;
-        if (&leftHandSide == nullptr)
+        if (&left_hand_side == nullptr)
             return false;
 
         if (!ignoreCase)
-            return leftHandSide == rightHandSide;
+            return left_hand_side == right_hand_side;
 
         const auto locale = std::locale();
 
@@ -38,17 +39,20 @@ namespace std
         };
 
         return std::equal(
-            begin(leftHandSide), end(leftHandSide), 
-            begin(rightHandSide), end(rightHandSide), 
+            begin(left_hand_side), end(left_hand_side), 
+            begin(right_hand_side), end(right_hand_side), 
             pred);
     }
-    [[nodiscard]] inline bool string_equal(std::string_view const leftHandSide, std::wstring_view const rightHandSide, bool ignoreCase /*= false */)
+    template <typename TCHAR>
+    [[nodiscard]] bool string_equal(std::basic_string<TCHAR> const& left_hand_side, std::basic_string<TCHAR> const &right_hand_side, bool const ignoreCase = false)
     {
-        if constexpr (&leftHandSide == nullptr && &rightHandSide == nullptr)
-            return true;
-        if constexpr (&leftHandSide == nullptr)
-            return false;
+        std::basic_string_view<TCHAR> const leftView(left_hand_side);
+        std::basic_string_view<TCHAR> const rightView(right_hand_side);
+        return string_equal(leftView, rightView, ignoreCase);
+    }
 
+    [[nodiscard]] inline bool string_equal(std::string_view const left_hand_side, std::wstring_view const right_hand_side, bool const ignoreCase = false)
+    {
         const auto locale = std::locale();
 
         if (ignoreCase)
@@ -60,8 +64,8 @@ namespace std
             };
 
             return std::equal(
-                begin(leftHandSide), end(leftHandSide), 
-                begin(rightHandSide), end(rightHandSide), 
+                begin(left_hand_side), end(left_hand_side), 
+                begin(right_hand_side), end(right_hand_side), 
                 pred);
         }
         else
@@ -73,27 +77,42 @@ namespace std
             };
 
             return std::equal(
-                begin(leftHandSide), end(leftHandSide), 
-                begin(rightHandSide), end(rightHandSide), 
+                begin(left_hand_side), end(left_hand_side), 
+                begin(right_hand_side), end(right_hand_side), 
                 pred);
         }
     }
-    [[nodiscard]] inline bool string_equal(std::wstring_view const leftHandSide, std::string_view const rightHandSide, bool ignoreCase /*= false */)
+    [[nodiscard]] inline bool string_equal(std::wstring_view const left_hand_side, std::string_view const right_hand_side, bool const ignoreCase = false)
     {
-        return string_equal(rightHandSide, leftHandSide, ignoreCase);
+        return string_equal(right_hand_side, left_hand_side, ignoreCase);
+    }
+
+    [[nodiscard]] inline bool string_equal(std::string const& left_hand_side, std::wstring const& right_hand_side, bool const ignoreCase = false)
+    {
+        std::string_view const leftView(left_hand_side);
+        std::wstring_view const rightView(right_hand_side);
+        return string_equal(leftView, rightView, ignoreCase);
+    }
+    [[nodiscard]] inline bool string_equal(std::wstring const& left_hand_side, std::string const& right_hand_side, bool const ignoreCase = false)
+    {
+        return string_equal(right_hand_side, left_hand_side, ignoreCase);
     }
 
     template <typename TCHAR>
     [[nodiscard]] std::vector<std::basic_string_view<TCHAR>> string_split(std::basic_string_view<TCHAR> const& value, std::vector<TCHAR> const& seperators)
     {
-        if (seperators.empty() || value.empty())
+        if (value.empty())
             return std::vector<std::basic_string_view<TCHAR>>();
+        if (seperators.empty())
+            return std::vector<std::basic_string_view<TCHAR>>{value};
 
         std::vector<std::basic_string_view<TCHAR>> parts{};
 
         size_t currentIndex{};
+        size_t i{0};
+        size_t const length{value.size()};
 
-        for (size_t i = 0, length = value.size(), seperatorCount = seperators.size(); i < length; i++)
+        for (; i < length; i++)
         {
             auto const& match = std::find_if(begin(seperators), end(seperators), [valueAtIndex = value[i]](auto const& seperator)
             {
@@ -105,10 +124,10 @@ namespace std
             {
                 parts.push_back(value.substr(currentIndex, partLength));
                 currentIndex = i + 1;
-                break;
             }
         }
-
+        if (currentIndex < length - 1)
+            parts.push_back(value.substr(currentIndex,  length - currentIndex));
         return parts;
     }
 
